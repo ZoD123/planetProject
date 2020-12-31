881a349786781308541cd0e0ba8ff23535712cde
Ina <ina.eisenkolb@gmail.com>
2020-12-31 11:18:38 +0100

    adding plant cell

--- src/com/eisenglatz/Main.java
+++ src/com/eisenglatz/Main.java
@@ -17,6 +17,7 @@ public class Main {
         ArrayList<Resource> seed = new ArrayList<Resource>();
         seed.add(new Oxygen(4000));
         seed.add(new Carbon(8000));
+//        seed.add(new CarbonDioxide(2));
         Integer cycleCount = 0;
         HashMap<String, Planet> Sol = new HashMap<String, Planet>();
         Planet newPlanet = new Planet("Earth", seed);
@@ -41,9 +42,9 @@ public class Main {
      * infests the given planet with life
      * @param planet
      */
-    private static void infest(Planet planet)
-    {
-        Organism simpleOrganism = new AnimalCell(planet);
+    private static void infest(Planet planet) {
+        Organism simpleAnimalCell = new AnimalCell(planet);
+        Organism simplePlantCell = new PlantCell(planet);
     }
 
     /**
---
+++ src/com/eisenglatz/PlantCell.java
@@ -0,0 +1,16 @@
+package com.eisenglatz;
+
+public class PlantCell extends Organism{
+
+    /**
+     * public constructor
+     * @param planet defines the location where the organism lives
+     *               and also defines which resources are available when metabolism get initialized
+     */
+    public PlantCell(Planet planet) {
+        super(planet);
+        super.requiredResource.put(CarbonDioxide.class,1);
+        super.producedResource.put(Oxygen.class,2);
+        super.producedResource.put(Carbon.class,1);
+    }
+}
